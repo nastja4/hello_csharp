4bcf128802488c33449628ee213efdce7fd51bc7
Fazius <Fazius@TCI.local>
2025-02-20 11:26:48 +0100

    Develop foreach and if-elseif-else structures to process array data in C#

--- Program.cs
+++ Program.cs
@@ -1,43 +1,100 @@
-﻿// to optimize
-/*
-int[] numbers = { 4, 8, 15, 16, 23, 42 };
+﻿int examAssignments = 5;
 
-foreach (int number in numbers)
-{
-    int total;
+// Student names
+string[] studentNames = ["Sophia", "Andrew", "Emma", "Logan"];
 
-    total += number;
+int[] sophiaScores = [90, 86, 87, 98, 100, 94, 90];
+int[] andrewScores = [92, 89, 81, 96, 90, 89];
+int[] emmaScores = [90, 85, 87, 98, 68, 89, 89, 89];
+int[] loganScores = [90, 95, 87, 88, 96, 96];
 
-    if (number == 42)
+int[] studentScores = [];
+string currentStudentLetterGrade = "";
+
+Console.WriteLine("Student\t\tExam Score\tOverall Grade\tExtra Credit\n");
+
+foreach (string name in studentNames)
+{   
+    if (name == "Sophia")
+        studentScores = sophiaScores;
+    else if (name == "Andrew")
+        studentScores = andrewScores;
+    else if (name == "Emma")
+        studentScores = emmaScores;
+    else if (name == "Logan")
+        studentScores = loganScores;
+
+    int gradedAssignments = 0;
+    int gradedExtraCreditAssignments = 0; 
+
+    int sumExamScores = 0;
+    int sumExtraCreditScores = 0;
+
+    decimal currentStudentGrade = 0;
+    decimal currentStudentExamScore = 0;
+    decimal currentStudentExtraCreditScore = 0;
+    
+    foreach (int score in studentScores)
     {
-       bool found = true;
-
+        gradedAssignments ++;
+        if (gradedAssignments <= examAssignments)
+        {
+            sumExamScores += score; 
+        }
+        else 
+        {
+            gradedExtraCreditAssignments ++;
+            sumExtraCreditScores += score;   
+        }                 
     }
 
+    currentStudentExamScore = (decimal)sumExamScores / examAssignments;
+    currentStudentExtraCreditScore = (decimal)sumExtraCreditScores / gradedExtraCreditAssignments;     
+
+    currentStudentGrade = (sumExamScores + ((decimal)sumExtraCreditScores / 10)) / examAssignments;
+
+    if (currentStudentGrade >= 97)
+        currentStudentLetterGrade = "A+";
+
+    else if (currentStudentGrade >= 93)
+        currentStudentLetterGrade = "A";
+
+    else if (currentStudentGrade >= 90)
+        currentStudentLetterGrade = "A-";
+
+    else if (currentStudentGrade >= 87)
+        currentStudentLetterGrade = "B+";
+
+    else if (currentStudentGrade >= 83)
+        currentStudentLetterGrade = "B";
+
+    else if (currentStudentGrade >= 80)
+        currentStudentLetterGrade = "B-";
+
+    else if (currentStudentGrade >= 77)
+        currentStudentLetterGrade = "C+";
+
+    else if (currentStudentGrade >= 73)
+        currentStudentLetterGrade = "C";
+
+    else if (currentStudentGrade >= 70)
+        currentStudentLetterGrade = "C-";
+
+    else if (currentStudentGrade >= 67)
+        currentStudentLetterGrade = "D+";
+
+    else if (currentStudentGrade >= 63)
+        currentStudentLetterGrade = "D";
+
+    else if (currentStudentGrade >= 60)
+        currentStudentLetterGrade = "D-";
+
+    else
+        currentStudentLetterGrade = "F";
+    
+    // Console.WriteLine($"{name}\t\t{currentStudentGrade}\t{currentStudentLetterGrade}");     
+    Console.WriteLine($"{name}\t\t{currentStudentExamScore}\t\t{currentStudentGrade}\t{currentStudentLetterGrade}\t{currentStudentExtraCreditScore} ({(decimal)sumExtraCreditScores / 10 / examAssignments} pts)");
 }
 
-if (found) 
-{
-    Console.WriteLine("Set contains 42");
-
-}
-
-Console.WriteLine($"Total: {total}");
-*/
-
-
-int[] numbers = [4, 8, 15, 16, 23, 42];
-int total = 0;
-bool found = false;
-
-foreach (int number in numbers)
-{
-    total += number;
-    if (number == 42)
-        found = true;
-}
-
-if (found)
-    Console.WriteLine("Set contains 42");
-
-Console.WriteLine($"Total: {total}");
+Console.WriteLine("Press the Enter key to continue");
+Console.ReadLine();
