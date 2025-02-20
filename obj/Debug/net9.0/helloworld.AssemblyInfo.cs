ee8ee31855890de0393c1bf0a51e158425e13f94
Fazius <Fazius@TCI.local>
2025-02-20 19:21:52 +0100

    Switch statement

--- obj/Debug/net9.0/helloworld.AssemblyInfo.cs
+++ obj/Debug/net9.0/helloworld.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("helloworld")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e0be36be35d5201baeb270bc064dd71a870ef010")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d834ca80c40fc97fddfc761d6c879482c6d2ad92")]
 [assembly: System.Reflection.AssemblyProductAttribute("helloworld")]
 [assembly: System.Reflection.AssemblyTitleAttribute("helloworld")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
