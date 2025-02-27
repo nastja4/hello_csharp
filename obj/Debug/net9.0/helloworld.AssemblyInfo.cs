a801e4a1a93333321cb51588660997b65e9d57f0
Fazius <chetaa.de@gmail.com>
2025-02-27 09:30:44 +0100

    Clear() and Resize()

--- obj/Debug/net9.0/helloworld.AssemblyInfo.cs
+++ obj/Debug/net9.0/helloworld.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("helloworld")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f4f571ef63f6f8bda0718564c62afad06b5ea7ef")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f97f1fb04e2a21710663b379f70e72340b9cf1d4")]
 [assembly: System.Reflection.AssemblyProductAttribute("helloworld")]
 [assembly: System.Reflection.AssemblyTitleAttribute("helloworld")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
