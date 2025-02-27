f25fb3631b56fdc86bc90bd5ab470bc48f5d0b48
Fazius <chetaa.de@gmail.com>
2025-02-27 15:50:56 +0100

    String formatting basics

--- Program.cs
+++ Program.cs
@@ -1,9 +1,23 @@
-﻿string orderStream = "B123,C234,A345,C15,B177,G3003,C235,B179";
+﻿using System.Globalization;
+using System.Text;
 
-string[] orderIDsArray = orderStream.Split(',');
-Array.Sort(orderIDsArray);
+// Set console encoding to UTF-8
+Console.OutputEncoding = Encoding.UTF8;
+CultureInfo germanCulture = new("de-DE");
 
-foreach (var order in orderIDsArray)
-{
-    Console.WriteLine(order.Length == 4 ? order : (order + "\t- Error"));     
-}
+
+decimal price = 123.45m;
+int discount = 50;
+Console.WriteLine($"Price: {price:C} (Save {discount:C})\n");
+
+decimal measurement = 123456.78912m;
+Console.WriteLine($"Measurement: {measurement:N} units\n");
+
+decimal tax = .36785m;
+Console.WriteLine($"Tax rate: {tax:P2}\n");
+
+decimal price2 = 67.55m;
+decimal salePrice = 59.99m;
+string yourDiscount = String.Format("You saved {0:C2} off the regular {1:C2} price. ", (price2 - salePrice).ToString("C", germanCulture), price2.ToString("C", germanCulture));
+yourDiscount += $"A discount of {((price2 - salePrice)/price2):P2}!";
+Console.WriteLine(yourDiscount);
