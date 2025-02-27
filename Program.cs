f482836cd873dcabcea65e3fa1dfcdbe401d8d97
Fazius <chetaa.de@gmail.com>
2025-02-27 10:14:39 +0100

    Split() and Join()

--- Program.cs
+++ Program.cs
@@ -1,30 +1,12 @@
-﻿string[] pallets = [ "B14", "A11", "B12", "A13" ];
-Console.WriteLine("");
+﻿string value = "abc123";
+char[] valueArray = value.ToCharArray();
+Array.Reverse(valueArray);
+// string result = new(valueArray);
+string result = string.Join(",", valueArray);
+Console.WriteLine(result);
 
-Array.Clear(pallets, 0, 2);
-Console.WriteLine($"Clearing 2 ... count: {pallets.Length}");
-foreach (var pallet in pallets)
-{    
-    Console.WriteLine($"-- {pallet}");
-}
-
-Console.WriteLine("");
-Array.Resize(ref pallets, 6);
-Console.WriteLine($"Resizing 6 ... count: {pallets.Length}");
-
-pallets[4] = "C01";
-pallets[5] = "C02";
-
-foreach (var pallet in pallets)
-{    
-    Console.WriteLine($"-- {pallet}");
-}
-
-Console.WriteLine("");
-Array.Resize(ref pallets, 3);
-Console.WriteLine($"Resizing 3 ... count: {pallets.Length}");
-
-foreach (var pallet in pallets)
+string[] items = result.Split(',');
+foreach (string item in items)
 {
-    Console.WriteLine($"-- {pallet}");
+    Console.WriteLine(item);
 }
--- obj/Debug/net9.0/helloworld.AssemblyInfo.cs
+++ obj/Debug/net9.0/helloworld.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("helloworld")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f97f1fb04e2a21710663b379f70e72340b9cf1d4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a801e4a1a93333321cb51588660997b65e9d57f0")]
 [assembly: System.Reflection.AssemblyProductAttribute("helloworld")]
 [assembly: System.Reflection.AssemblyTitleAttribute("helloworld")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
