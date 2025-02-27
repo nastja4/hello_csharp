f4f571ef63f6f8bda0718564c62afad06b5ea7ef
Fazius <chetaa.de@gmail.com>
2025-02-27 07:41:42 +0100

    Specific number types

--- obj/Debug/net9.0/helloworld.AssemblyInfo.cs
+++ obj/Debug/net9.0/helloworld.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("helloworld")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0702b16ea1e17dff8811d5c366b021a4816fd5d4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+38fceeff2ef5acb0e73af46a0c4ce1417e27ca8a")]
 [assembly: System.Reflection.AssemblyProductAttribute("helloworld")]
 [assembly: System.Reflection.AssemblyTitleAttribute("helloworld")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
