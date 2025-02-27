3684d6e292d0e86fe087b4c697e54ab5ebec42c8
Fazius <chetaa.de@gmail.com>
2025-02-27 13:43:20 +0100

    Parse a string of orders, sort the orders and tag possible errors

--- Program.cs
+++ Program.cs
@@ -1,35 +1,9 @@
-﻿string pangram = "The quick brown fox jumps over the lazy dog";
+﻿string orderStream = "B123,C234,A345,C15,B177,G3003,C235,B179";
 
-// Step 1: Split the sentence into words
-string[] words = pangram.Split(' ');
+string[] orderIDsArray = orderStream.Split(',');
+Array.Sort(orderIDsArray);
 
-// Step 2: Reverse each word using a loop and Array methods
-for (int i = 0; i < words.Length; i++)
+foreach (var order in orderIDsArray)
 {
-    // Convert word to char array, reverse it, and convert back to string
-    char[] charArray = words[i].ToCharArray();
-    Array.Reverse(charArray);
-    words[i] = new string(charArray);
+    Console.WriteLine(order.Length == 4 ? order : (order + "\t- Error"));     
 }
-
-// Step 3: Join the modified words back into a sentence
-string result = string.Join(" ", words);
-
-// Print the result
-Console.WriteLine(result);
-
-/*
-string pangram = "The quick brown fox jumps over the lazy dog";
-
-string[] words = pangram.Split(' ');
-
-for (int i = 0; i < words.Length; i++)
-{
-    char[] charArray = words[i].ToCharArray();
-    Array.Reverse(charArray);
-    words[i] = new string(charArray);
-}
-
-string result = string.Join(" ", words);
-Console.WriteLine(result);
-*/
