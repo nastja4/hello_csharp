6d6ead563610a43def3c47c5b5a92a278a5dfde2
Fazius <chetaa.de@gmail.com>
2025-02-28 15:57:30 +0100

    Work with different types of symbol sets with IndexOfAny()

--- Program.cs
+++ Program.cs
@@ -1,23 +1,37 @@
-﻿string message = "(What if) I am (only interested) in the last (set of parentheses)?";
+﻿string message = "(What if) I have [different symbols] but every {open symbol} needs a [matching closing symbol]?";
 
+char[] openSymbols = ['[', '{', '('];
+
+// Keeps track of where the last matching closing symbol was found
+int closingPosition = 0;
+
+// Looping through the string
 while (true)
 {
-    int openingPosition = message.IndexOf('(');
+    int openingPosition = message.IndexOfAny(openSymbols, closingPosition);
+
     if (openingPosition == -1) break;
 
-    openingPosition += 1;
-    int closingPosition = message.IndexOf(')');
+    // Determining the matching closing symbol
+    string currentSymbol = message.Substring(openingPosition, 1);
+    char matchingSymbol = ' ';
+    switch (currentSymbol)
+    {
+        case "[":
+            matchingSymbol = ']';
+            break;
+        case "{":
+            matchingSymbol = '}';
+            break;
+        case "(":
+            matchingSymbol = ')';
+            break;
+    }
+
+    // Finding the closing symbol & extracting the content
+    openingPosition += 1;  // Move to the start of the enclosed content
+    closingPosition = message.IndexOf(matchingSymbol, openingPosition);
+
     int length = closingPosition - openingPosition;
     Console.WriteLine(message.Substring(openingPosition, length));
-
-    // Note the overload of the Substring to return only the remaining 
-    // unprocessed message:
-    message = message.Substring(closingPosition + 1);
 }
-
-/*
-What if
-only interested
-set of parentheses
-*/
-
