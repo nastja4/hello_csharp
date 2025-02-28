fec582dcf32515c4a82763bd91662ad72b819718
Fazius <chetaa.de@gmail.com>
2025-02-28 10:56:34 +0100

    Solution to string interpolation

--- Program.cs
+++ Program.cs
@@ -2,16 +2,51 @@
 Console.OutputEncoding = Encoding.UTF8;
 
 
-string paymentId = "769C";
-string payeeName = "Mr. Stephen Ortega";
-string paymentAmount = "5.000,00 €";
+string customerName = "Ms. Barros";
 
-var formattedLine = paymentId.PadRight(6);
-formattedLine += payeeName.PadRight(24);
-formattedLine += paymentAmount.PadRight(10);
+string currentProduct = "Magic Yield";
+int currentShares = 2975000;
+decimal currentReturn = 0.1275m;
+decimal currentProfit = 55000000.0m;
 
-Console.WriteLine("1234567890123456789012345678901234567890");
-Console.WriteLine(formattedLine); 
+string newProduct = "Glorious Future";
+decimal newReturn = 0.13125m;
+decimal newProfit = 63000000.0m;
 
-//1234567890123456789012345678901234567890
-//769C  Mr. Stephen Ortega      5.000,00 €
+// Your logic here
+Console.WriteLine($"Dear {customerName},");
+Console.WriteLine($"As a customer of our {currentProduct} offering we are excited to tell you about a new financial product that would dramatically increase your return.\n");
+Console.WriteLine($"Currently, you own {currentShares:N} shares at a return of {currentReturn:P}.\n");
+Console.WriteLine($"Our new product, {newProduct} offers a return of {newReturn:P}.  Given your current volume, your potential profit would be {newProfit:C}.\n");
+// 
+
+Console.WriteLine("Here's a quick comparison:\n"); 
+
+string comparisonMessage = "";
+
+// Your logic here
+comparisonMessage = currentProduct.PadRight(20);
+comparisonMessage += string.Format("{0:P}", currentReturn).PadRight(10);
+comparisonMessage += string.Format("{0:C}", currentProfit).PadRight(20);
+
+comparisonMessage += "\n";
+comparisonMessage += newProduct.PadRight(20);
+comparisonMessage += string.Format("{0:P}", newReturn).PadRight(10);
+comparisonMessage += string.Format("{0:C}", newProfit).PadRight(20);
+//
+
+Console.WriteLine(comparisonMessage);
+
+/*
+Dear Ms. Barros,
+As a customer of our Magic Yield offering we are excited to tell you about a new financial product that would dramatically increase your return.
+
+Currently, you own 2.975.000,00 shares at a return of 12,75 %.
+
+Our new product, Glorious Future offers a return of 13,13 %.  Given your current volume, your potential profit would be 63.000.000,00 €.
+
+Here's a quick comparison:
+
+Magic Yield         12,75 %   55.000.000,00 €
+Glorious Future     13,13 %   63.000.000,00 €
+*/
--- obj/Debug/net9.0/helloworld.AssemblyInfo.cs
+++ obj/Debug/net9.0/helloworld.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("helloworld")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9c900a4abcb644c7ecf7e1c167a7881a81dd1b83")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+17a0ca936179f9c8fb759a494949613fe06ba4be")]
 [assembly: System.Reflection.AssemblyProductAttribute("helloworld")]
 [assembly: System.Reflection.AssemblyTitleAttribute("helloworld")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
